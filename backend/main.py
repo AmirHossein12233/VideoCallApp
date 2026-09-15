76439aafc0285b436bf07eb01ca08c7332d3801d
Amir Hossein <nasryzdany0879@gmail.com>
2026-09-15 12:19:52 +0330

    Fix Android API CORS

--- backend/main.py
+++ backend/main.py
@@ -26,14 +26,7 @@ app = FastAPI(
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=[
-        "https://videocallapp-web.onrender.com",
-        "http://127.0.0.1:5500",
-        "http://localhost:5500",
-        "https://localhost",
-        "http://localhost",
-        "capacitor://localhost",
-    ],
+    allow_origins=["*"],
     allow_credentials=False,
     allow_methods=["*"],
     allow_headers=["*"],
